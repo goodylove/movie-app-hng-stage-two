9fb2ee5b5d1895c833f77248b77fbd9df95e9574
goodness <goodyc474@gmail.com>
2023-09-11 07:06:23 +0100

    ft: add nav search and mobile ui and toogle functionality

---
+++ src/Components/searchInput/index.jsx
@@ -0,0 +1,16 @@
+import { Icons } from "./../../constants/icons";
+
+function Search() {
+  return (
+    <div className=" text-white flex items-center border-2 border-white rounded-md w-[525px] justify-between p-3 h-[36px]  max-w-[100%]">
+      <input
+        type="search"
+        placeholder="what do you want to watch"
+        className=" bg-transparent text-white  outline-none border-none "
+      />
+      <span>{Icons.searchIcon()}</span>
+    </div>
+  );
+}
+
+export default Search;
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,44 +1,73 @@
 import { Icons } from "./../../constants/icons";
+import Search from "./../../Components/searchInput/index";
+import { useState } from "react";
+
 function Home() {
+  const [toggleNavItem, setToggleNavItem] = useState(false);
+
+  // this hanlder controls the toggling of Nav items for both destop and mobile devices
+  const handleToogleNavItem = () => {
+    setToggleNavItem((prev) => !prev);
+  };
+
+  // removes the mobile search component
+  const handleRemoveMobileSearch = () => {
+    setToggleNavItem(false);
+  };
+
   return (
     <main>
       {/* hero section */}
 
       <section className="hero">
         {/* nav section */}
+
         <nav className="flex justify-between md:py-3 md:px-10 items-center p-3 relative">
           {/* movie logo */}
-          <div className=" text-white flex items-center md:gap-4 gap-2 text-[10px]  md:text-[18px]">
-            <span>{Icons.movieLogo()}</span>
-            <span>MovieBox</span>
-          </div>
-          {/* search section */}
-          <div className=" text-white md:flex items-center border-2 border-white rounded-md w-[525px] justify-between p-3 h-[36px] hidden">
-            <input
-              type="search"
-              placeholder="what do you want to watch"
-              className=" bg-transparent text-white w-full outline-none border-none "
-            />
-            <span>{Icons.searchIcon()}</span>
-          </div>
-          {/* mobile search section */}
-          <div>
-            <input
-              type="search"
-              placeholder="what do you want to watch"
-              className=" bg-transparent text-white w-[60%] rounded-md p-1 outline-none   absolute top-20 border-2 left-20"
-            />
-          </div>
+
+          {toggleNavItem ? (
+            ""
+          ) : (
+            <div className=" text-white flex items-center md:gap-4 gap-2 text-[10px]  md:text-[18px]">
+              <span>{Icons.movieLogo()}</span>
+              <span className="md:text-[17px] text-[14px]">MovieBox</span>
+            </div>
+          )}
+
+          {/*  desktop search section */}
+          <span className="hidden">
+            <Search />
+          </span>
 
           {/* signin */}
-          <div className="text-white flex items-center md:text-[18px]  text-[10px] md:gap-4 gap-2">
-            <span className="md:hidden flex cursor-pointer">
-              {Icons.searchIcon()}
-            </span>
-            <span>Sign In </span>
-            <span>{Icons.hambugerIcon()}</span>
-          </div>
+          {toggleNavItem ? (
+            ""
+          ) : (
+            <div className="text-white flex items-center md:text-[18px]  text-[10px] md:gap-4 gap-2">
+              <span
+                className="md:hidden flex cursor-pointer"
+                onClick={handleToogleNavItem}
+              >
+                {Icons.searchIcon()}
+              </span>
+              <span className="md:flex hidden">Sign In </span>
+              <span>{Icons.hambugerIcon()}</span>
+            </div>
+          )}
         </nav>
+        <div>
+          {/* mobile search section */}
+          {toggleNavItem ? (
+            <div className=" md:hidden  flex  w-full items-center  justify-between py-3 px-1 gap-3  ">
+              <Search />
+              <span className="text-white" onClick={handleRemoveMobileSearch}>
+                Cancel
+              </span>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
       </section>
     </main>
   );
