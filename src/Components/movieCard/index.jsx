2c8c8dbb378ca12b0dbcecc33158c8a3261dcf7e
goodness <goodyc474@gmail.com>
2023-09-14 22:22:23 +0100

    ch: remove view more button

--- src/Components/movieCard/index.jsx
+++ src/Components/movieCard/index.jsx
@@ -23,65 +23,61 @@ function MovieCard({ item }) {
 
   return (
     // movie card
-    <div
-      className="w-[250px] h-[490px] cursor-pointer"
-      data-testid="movie-card"
-    >
-      {/* favourite icon */}
-
+    <Link to={`/movies/${item.id}`}>
       <div
-        className=" bg-slate-400 h-5  w-5 rounded-full  flex justify-center items-center   absolute   ml-48 mt-6  cursor-pointer"
-        onClick={handleSaveFavorites}
+        className="w-[250px] h-[490px] cursor-pointer"
+        data-testid="movie-card"
       >
-        {toogleFavoriteIcon ? (
-          <MdFavorite className="" />
-        ) : (
-          <MdFavoriteBorder className="text-white text-md     " />
-        )}
-      </div>
+        {/* favourite icon */}
 
-      {/* movie poster */}
-      <img
-        src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
-        alt={item.title}
-        className="w-[250px] h-[370px] "
-        data-testid="movie-poster"
-      />
-
-      {/* movie release data */}
-      <h4
-        className="text-[12px] leading-[15.62px] my-3"
-        data-testid="movie-release-date"
-      >
-        {getDateInUtcFormate(item.release_date)}
-      </h4>
-
-      {/* movie title */}
-      <h2
-        className="leading-[23.44px] text-[18px] font-[700]"
-        data-testid="movie-title"
-      >
-        {item.title}
-      </h2>
-
-      <div className="flex gap-8 text-[10px] my-2">
-        <div className="flex items-center gap-2">
-          <img src={ImbImg} alt="imb-img" />
-          <span>850/100</span>
+        <div
+          className=" bg-slate-400 h-5  w-5 rounded-full  flex justify-center items-center   absolute   ml-48 mt-6  cursor-pointer"
+          onClick={handleSaveFavorites}
+        >
+          {toogleFavoriteIcon ? (
+            <MdFavorite className="" />
+          ) : (
+            <MdFavoriteBorder className="text-white text-md     " />
+          )}
         </div>
 
-        <div className="flex items-center gap-2">
-          <img src={PngItem} alt="pngImage" />
-          <span>{calcMovieRating(item.vote_average)}</span>
+        {/* movie poster */}
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+          alt={item.title}
+          className="w-[250px] h-[370px] "
+          data-testid="movie-poster"
+        />
+
+        {/* movie release data */}
+        <h4
+          className="text-[12px] leading-[15.62px] my-3"
+          data-testid="movie-release-date"
+        >
+          {getDateInUtcFormate(item.release_date)}
+        </h4>
+
+        {/* movie title */}
+        <h2
+          className="leading-[23.44px] text-[18px] font-[700]"
+          data-testid="movie-title"
+        >
+          {item.title}
+        </h2>
+
+        <div className="flex gap-8 text-[10px] my-2">
+          <div className="flex items-center gap-2">
+            <img src={ImbImg} alt="imb-img" />
+            <span>850/100</span>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <img src={PngItem} alt="pngImage" />
+            <span>{calcMovieRating(item.vote_average)}</span>
+          </div>
         </div>
       </div>
-
-      <Link to={`/movies/${item.id}`}>
-        <Button className="text-[12px] leading-[15.62px] bg-black text-white p-1 rounded">
-          View More
-        </Button>
-      </Link>
-    </div>
+    </Link>
   );
 }
 
